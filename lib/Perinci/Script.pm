1fef3ce60837305e8d0dbfdf835d5b40fb712c56
perlancar (on pc-office) <perlancar@gmail.com>
2018-12-06 20:19:47 +0700

    Hackety hack

--- lib/Perinci/Script.pm
+++ lib/Perinci/Script.pm
@@ -4,12 +4,40 @@ package Perinci::Script;
 # VERSION
 
 1;
-# ABSTRACT: Script framework to run functions described by Rinci
+# ABSTRACT: Run functions described by Rinci in various environment
 
 =head1 DESCRIPTION
 
-The C<Perinci::Script> namespace is used for script framework to run functions
-described by L<Rinci>.
+Perinci::Script is a framework to create scripts that can run your Perl
+functions (or functions created in other languages) over various environments,
+from CLI to CGI/FCGI/PSGI.
+
+ Perinci::Script::Base
+   Perinci::Script::CLI::Base          (old name: Perinci::CmdLine::Base)
+     Perinci::Script::CLI::Lite        (old name: Perinci::CmdLine::Lite)
+     Perinci::Script::CLI::Heavy       (old name: Perinci::CmdLine::Classic)
+     Perinci::Script::CLI::HTMLForm    for scripts invoked from the command-line but opens browser and presents the UI as HTML form. runs HTTP server locally.
+   Perinci::Script::CLI::Any           (old name: Perinci::CmdLine::Any)
+   Perinci::Script::CLI::CUI
+
+   Perinci::Script::CGI
+   Perinci::Script::CGI::API           for scripts to be run as CGI script, return JSON response
+   Perinci::Script::CGI::HTML          for scripts to be run as CGI script, presents HTML form and return HTML/JSON
+
+   Perinci::Script::FCGI
+   Perinci::Script::FCGI::API          for scripts to be run as FCGI script, return JSON response
+   Perinci::Script::FCGI::HTML         for scripts to be run as FCGI script, presents HTML form and return HTML/JSON
+
+   Perinci::Script::PSGI
+   Perinci::Script::PSGI::API          for scripts to be run as PSGI script, return JSON response
+   Perinci::Script::PSGI::HTML         for scripts to be run as PSGI script, presents HTML form and return HTML/JSON
+
+   Perinci::Script::CUI?
+   Perinci::Script::GUI?
+
+ Perinci::Script::HTTP::Any?           for scripts to be run as CGI/FCGI/PSGI depending on the environment
+
+ Perinci::Script::Any                  for scripts to to be run as CLI, CGI, FCGI, or PSGI depending on the environment
 
 
 =head1 SEE ALSO
