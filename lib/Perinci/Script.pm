fc7a35acf05c18a91198e09aeb55c3607cc8321b
perlancar (on pc-office) <perlancar@gmail.com>
2018-12-06 18:35:50 +0700

    Initial commit

---
+++ lib/Perinci/Script.pm
@@ -0,0 +1,21 @@
+package Perinci::Script;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: Script framework to run functions described by Rinci
+
+=head1 DESCRIPTION
+
+The C<Perinci::Script> namespace is used for script framework to run functions
+described by L<Rinci>.
+
+
+=head1 SEE ALSO
+
+L<Rinci>
+
+L<Riap>
+
+=cut
